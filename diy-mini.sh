ea6f3f18269e803647970a95e3b881e72aa41316
littletao12 <166675326+littletao12@users.noreply.github.com>
2025-02-12 19:42:53 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -41,7 +41,7 @@ git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclas
 
 git clone --depth=1 https://github.com/littletao12/openwrt-bin package/openwrt-bin
 git clone --depth=1 https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki package/OpenWrt-nikki
 # Themes
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
